f425d6241106bf84d9ad9833117bec0bb2de6690
Martin <justanadress@windowslive.com>
2015-04-21 19:01:26 +0200

    Improved AddressAssistant

--- src/tools/AddressAssistant.java
+++ src/tools/AddressAssistant.java
@@ -1,6 +1,8 @@
 package tools;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.InetAddress;
 import java.net.URL;
 import java.net.URLConnection;
@@ -8,36 +10,47 @@ import java.net.UnknownHostException;
 
 
 public class AddressAssistant {
-    private static final String localAddressError = "Nie udało się ustalenie adresu lokalnego serwera.";
+    private static final String addressError = "Nie powiodło się ustalenie adresu serwera.";
 
     public static String getAddresses() {
-        String localAddress = getLocalAddress();
-        String externalAddress = getExternalAddress();
-        return externalAddress.equals("") ? localAddress : localAddress + "\n" + externalAddress;
+        String result;
+
+        String localAddress = getLocalAddress(); //"Lokalny adres IP serwera: " +
+        String externalAddress = getExternalAddress(); //" + ;
+
+        if(localAddress.equals("")) result = addressError;
+        else if(localAddress.equals(externalAddress)) result = "Adres IP serwera: " + localAddress;
+        else result = "Lokalny adres IP serwera: " + localAddress + " \nZewnętrzny adres IP serwera: " + externalAddress;
+
+        return result;
     }
 
-    private static String getLocalAddress(){
-        String result = "Lokalny adres IP serwera: ";
+    private static String getLocalAddress() {
+        String result = "";
 
-        try{
-            result += InetAddress.getLocalHost().getHostAddress().toString();
-        } catch (UnknownHostException exception) {
-            result = localAddressError;
+        try {
+            result += InetAddress.getLocalHost().getHostAddress();
+        } catch (UnknownHostException ignored) {
         }
 
         return result;
     }
 
-    private static String getExternalAddress(){
+    private static String getExternalAddress() {
         String result = "";
 
-        try{
+        try {
             URL url = new URL("http://checkip.amazonaws.com");
             URLConnection connection = url.openConnection();
+
             connection.setConnectTimeout(1500);
             connection.setReadTimeout(1500);
-            result += connection.getInputStream().read();
-        } catch (IOException exception) {}
+
+            result += new BufferedReader(new InputStreamReader(
+                    connection.getInputStream())).readLine();
+
+        } catch (IOException ignored) {
+        }
 
         return result;
     }
